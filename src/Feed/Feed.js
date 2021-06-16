02896f4b3662c84a0d137e44af81ffcc6ab6ab8c
Mac <ricardofer2412@hotmail.com>
2021-06-16 11:24:08 -0400

    fix error on feed component

--- src/Feed/Feed.js
+++ src/Feed/Feed.js
@@ -1,16 +1,17 @@
-import React, { useState, useEffect } from 'react'
-import './feed.css'
-import Tweetbox from './Tweetbox'
-import Post from './Post'
-import db from 'firebase'
+import React, { useState, useEffect } from "react";
+import Tweetbox from "./Tweetbox";
+import Post from "./Post";
+import "./feed.css";
+import db from "../Firebase/Firebase";
+
 
 function Feed() {
-  const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    db.collection('posts').onSnapshot(snapshot => (
-      setPosts(snapshot.docs.map(doc => doc.data()))
-    ))
+    db.collection("posts").onSnapshot((snapshot) =>
+      setPosts(snapshot.docs.map((doc) => doc.data()))
+    );
   }, []);
   return (
     <div className='feed'>
@@ -19,15 +20,11 @@ function Feed() {
 
       </div>
       <Tweetbox />
-      {posts.map(posts => ())}
-      <Post
-        displayName=' Richard Casanova'
-        username='richardcasanova'
-        virified={true}
-        text='This is my first tweet on my Twitter Clone Web App'
-        avatar='https://scontent.fmia1-1.fna.fbcdn.net/v/t1.0-9/37774916_10155371735512015_3382276897239990272_n.jpg?_nc_cat=103&_nc_sid=09cbfe&_nc_ohc=QBGhXrk-DxsAX93JtKh&_nc_ht=scontent.fmia1-1.fna&oh=3cad4f17a0a329ef854f84559180ce9d&oe=5F70E051'
-        image='https://media.giphy.com/media/Dh9jqNm8YcGVG/giphy.gif'
-      />
+
+      {posts.map(posts => (
+         <Post />
+      )  )}
+     
       <Post />
       <Post />
       <Post />
